9d536e0239f5fd131f5ac83dce03a30a9f10cccb
muschellij2 <muschellij2@gmail.com>
2016-07-19 14:09:05 -0400

    Added unit option for voxres.

--- R/voxres.R
+++ R/voxres.R
@@ -1,9 +1,15 @@
 #' @title Gets Voxel Resolution
-#' @return Scalar numeric, one number, in cubic mm.
+#' @return Scalar numeric, one number, in cubic mm or cubic cm (cc/mL).
 #' @param img nifti object
+#' @param units output unit, either cubic mm or cubic cm.
 #' @description Grabs the 3 voxel dimensions and takes the product
 #' @export
-voxres = function(img){
-  prod(voxdim(img))
+voxres = function(img, units = c("mm", "cm")){
+  units = match.arg(units)
+  divisor = switch(
+    "mm" = 1,
+    "cm" = 1000
+  )
+  prod(voxdim(img))/divisor
 }
 
