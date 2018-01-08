f3960af16e5b548ada054f07fca2a923cebdc0e1
muschellij2 <muschellij2@gmail.com>
2018-01-08 14:51:35 -0500

    fixing travis to coveralls

--- tests/testthat/test_anlz.R
+++ tests/testthat/test_anlz.R
@@ -10,3 +10,32 @@ test_that("operators", {
   expect_true(is.anlz(img02 / pi))
 })
 
+
+test_that("extracting some info", {
+  expect_silent(cal.units(img01))
+  expect_silent({
+    cal.units(img01) = "hey"
+    })
+  expect_equal(cal.units(img01), "hey")
+  
+  expect_silent({
+    cal_units(img01) = "ho"
+  })
+  expect_equal(cal_units(img01), "ho")
+  
+})
+
+# db_name
+test_that("extract and assign", {
+  funcs = "db_name"
+  func = funcs[1]
+  assign_func = paste0(func, "<-")
+  t_value = basename(tempfile())
+  expect_silent(do.call(func, args = list(img01)))
+  expect_silent({
+    img01 = do.call(assign_func, list(object = img01, value = t_value))
+  })
+  expect_equal(do.call(func, list(img01)), t_value)
+})
+
+
