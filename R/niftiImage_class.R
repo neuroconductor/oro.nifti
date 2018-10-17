ae3cf9b9b35fa5d781fd9d2575ad78f263d42a5c
muschellij2 <muschellij2@gmail.com>
2018-10-17 17:30:50 -0400

    niftiImage fully implemented for the accessor functions

---
+++ R/niftiImage_class.R
@@ -0,0 +1,18 @@
+#' "niftiImage" class
+#'
+#' @name niftiImage-class
+#' @aliases niftiImage
+#' @family niftiImage
+#' 
+#' @import methods
+#'
+#' @exportClass niftiImage
+setOldClass("niftiImage")
+
+#' @name internalImage-class
+#' @aliases internalImage
+#' 
+#' @rdname internalImage
+#' @exportClass internalImage
+setOldClass("internalImage")
+
